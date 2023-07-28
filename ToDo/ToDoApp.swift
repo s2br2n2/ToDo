a4d172a177ab61665fe1ebc967f2daee7e9784ff
Sabrina <s2br2n2@gmail.com>
2023-07-28 14:08:17 +0100

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by scholar on 28/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
